0f235a6c6a32faa824752560c8e5870d1082b832
Nune Siva Kumar <sivakumarnune978@gmail.com>
2026-03-02 00:00:46 +0530

    Create FiveRangeSeries.java

---
+++ FiveRangeSeries.java
@@ -0,0 +1,54 @@
+/* Write program to print the following series which is shown in Given Examples.
+Constraints:
+Input          :- First Line of Input Consists of One Integer Value.
+                     Second Line of Input Consists of One Integer Value.
+Output        :- Print the Following Series.
+Constraints  :- No */
+
+import java.util.Scanner;
+class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=0;
+        if(a>b)
+        {
+            for(int i=a;i>=b;i--)
+            {
+                c++;
+                if(c!=1)
+                {
+                    System.out.print(", ");
+                }
+                if(i>=0)
+                {
+                    System.out.print("5*"+i);
+                }
+                else
+                {
+                    System.out.print("5*("+i+")");
+                }
+            }
+        }
+        else
+        {
+            for(int i=a;i<=b;i++)
+            {
+                c++;
+                if(c!=1)
+                {
+                System.out.print(", ");
+                }
+                if(i>=0)
+                {
+                System.out.print("5*"+i);
+                }
+                else
+                {
+                System.out.print("5*("+i+")");
+                }
+            }
+        }
+    }
+}
