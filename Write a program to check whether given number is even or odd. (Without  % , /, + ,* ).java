87030bb47ff0cb5c4173dad30f3843f9d78558b8
Nune Siva Kumar <sivakumarnune978@gmail.com>
2026-03-03 17:02:17 +0530

    Create  , + ,* ).java

---
+++ Write a program to check whether given number is even or odd. (Without  % , /, + ,* ).java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class main{
+    public static void main(String[] args){
+        Scanner Sc = new Scanner (System.in);
+        int n = Sc.nextInt();
+        if(n<=0)
+        {
+            System.out.print("Invalid Input");
+        }
+        else
+        {
+            while(n>0)
+            {
+                n=n-2;
+            }
+            if(n==0)
+            {
+                System.out.print("Even");
+            }
+            else
+            {
+                System.out.print("Odd");
+            }
+        }
+    }
+}
