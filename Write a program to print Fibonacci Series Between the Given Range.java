0f4fa7b21c11b3cdc4fb2e4d9e8a0be50d5abd83
Nune Siva Kumar <sivakumarnune978@gmail.com>
2026-03-03 16:39:19 +0530

    Create Write a program to print Fibonacci Series Between the Given Range.java

---
+++ Write a program to print Fibonacci Series Between the Given Range.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+       int n1 = sc.nextInt();
+       int n2 = sc.nextInt();
+       int a = 0;
+       int b = 1;
+       int count =0;
+       if(n1<0 || n2<0)
+       {
+           System.out.print("Invalid Inputs");
+       }
+       else
+       {
+           if(n1>n2)
+           {
+               int temp = n1;
+               n1=n2;
+               n2=temp;
+           }
+           
+           //for(int i=a;i<n2;i++)
+           while(a<=n2)
+           {
+               if(a>=n1)
+               {
+                   System.out.print(a+" ");
+                   count++;
+               }
+               int c=a+b;
+               a=b;
+               b=c;
+           }
+           if(count==0)
+           {
+               System.out.print("No Fibonacci Series Values");
+           }
+           
+       }
+    }
+}
