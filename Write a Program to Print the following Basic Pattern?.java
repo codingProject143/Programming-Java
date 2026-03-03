f482122af954e0a6795d25ac83b7d8864233af4c
Nune Siva Kumar <sivakumarnune978@gmail.com>
2026-03-03 17:44:04 +0530

    Create Write a Program to Print the following Basic Pattern?.java

---
+++ Write a Program to Print the following Basic Pattern?.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n<=0)
+        {
+            System.out.print("Invalid Input");
+        }
+        else
+        {
+            for(int i=1;i<=n;i++)
+            {
+                for(int j=1;j<=n;j++)
+                {
+                    if(i+j==n+1)
+                    {
+                        System.out.print(i+" ");
+                    }
+                    else if(i+j<=n+1)
+                    {
+                        System.out.print("1 ");
+                    }
+                    else
+                    {
+                        System.out.print(i+" ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+        
+    }
+}
